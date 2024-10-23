58fcbbde1687afa5730d12eee9f0840a9564f0d0
Jonathan Mendes <carisjhow@gmail.com>
2024-10-23 00:46:09 -0300

    Configurando o grid da tabela que compôe o todapé

--- front/src/components/ContadorDeBolasLancadasPorOrdemDeChamada/index.tsx
+++ front/src/components/ContadorDeBolasLancadasPorOrdemDeChamada/index.tsx
@@ -1,31 +1,12 @@
+import "./style.css";
 const listade100numeros = Array.from({ length: 100 }, (_, i) => i + 1);
 
 export function ContadorDeBolasLancadasPorOrdemDeChamada(): JSX.Element {
   return (
-    <div
-      className="BolasDoRodapé"
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(25, 25px)", //Cria Dez Colunas
-        gridTemplateRows: "repeat(4, 25px)", //Cria Dez Linhas
-        gap: "20px",
-        boxShadow: "-2px -2px 5px var(--dark-grysdepayne)",
-      }}
-    >
+    <div className="BolasDoRodape">
       {listade100numeros.map((numero) => (
         <div
           key={numero}
-          style={{
-            border: "1px solid lightblue",
-            backgroundColor: "var(--light-petroleumblue)",
-            boxShadow: "2px 2px 5px var(--dark-black)",
-            borderRadius: "50%",
-            width: "30px",
-            height: "30px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
           onClick={() => {
             alert(`button ${numero} clicked`);
           }}
--- front/src/components/NumerosLancadosCartela/index.tsx
+++ front/src/components/NumerosLancadosCartela/index.tsx
@@ -1,31 +1,12 @@
+import "./style.css";
 const listade100numeros = Array.from({ length: 100 }, (_, i) => i + 1);
 
 export function NumerosLancadosCartela(): JSX.Element {
   return (
-    <div
-      className="NumerosLancadosCartelaGrid"
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(10, 25px)",
-        gridTemplateRows: "repeat(10, 25px)",
-        gap: "15px",
-        backgroundColor: "var(--light-petroleumblue)",
-        boxShadow: "-2px -2px 3px var(--dark-grysdepayne)",
-      }}
-    >
+    <div className="NumerosLancadosCartelaGrid">
       {listade100numeros.map((numero) => (
         <div
           key={numero}
-          style={{
-            border: "1px solid lightblue",
-            backgroundColor: "var(--light-blue)",
-            boxShadow: "2px 2px 5px var(--dark-black)",
-            borderRadius: "5px",
-            width: "30px",
-            height: "30px",
-            display: "flex",
-            justifyContent: "center",
-          }}
           onClick={() => {
             alert(`button ${numero} clicked`);
           }}
